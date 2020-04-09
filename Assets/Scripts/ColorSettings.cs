6c7e03b5db9edf3b9f2a2da1080ab9d01cd87ed1
npels <npels@berkeley.edu>
2020-04-08 21:07:25 -0700

    initial commit

---
+++ Assets/Scripts/ColorSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ColorSettings : ScriptableObject {
+
+    public Material planetMaterial;
+    public BiomeColorSettings biomeColorSettings;
+    public Gradient oceanColor;
+
+    [System.Serializable]
+    public class BiomeColorSettings {
+        public Biome[] biomes;
+        public NoiseSettings noise;
+        public float noiseOffset;
+        public float noiseStrength;
+        [Range(0, 1)]
+        public float blendAmount;
+
+        [System.Serializable]
+        public class Biome {
+            public Gradient gradient;
+            public Color tint;
+            [Range(0, 1)]
+            public float tintPercent;
+            [Range(0, 1)]
+            public float startHeight;
+        }
+    }
+}
