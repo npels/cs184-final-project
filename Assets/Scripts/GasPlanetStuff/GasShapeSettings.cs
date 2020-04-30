024b9fd11f001da29d7ca937ee7e48cc2db9f8a7
Lea Nakatsukasa <leajnaka@berkeley.edu>
2020-04-30 02:09:27 -0700

    Gas planet has rings

--- Assets/Scripts/GasPlanetStuff/GasShapeSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu()]
-public class GasShapeSettings : ScriptableObject
-{
-    public float planetRadius = 1;
-}
